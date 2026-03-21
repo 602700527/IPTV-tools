f871a4a4815d411aa9d40cc1b9e15850d3a892a9
yim <602700527@qq.com>
2026-03-22 02:42:09 +0800

    修复禁用直连播放下，收藏的频道也可以播放的bug

--- .wrangler/tmp/dev-IqJhVr/worker.js
+++ .wrangler/tmp/dev-IqJhVr/worker.js
@@ -9,7 +9,7 @@ var __export = (target, all) => {
     __defProp(target, name, { get: all[name], enumerable: true });
 };
 
-// .wrangler/tmp/bundle-Ehbdbw/checked-fetch.js
+// .wrangler/tmp/bundle-eDem2s/checked-fetch.js
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
     (typeof request === "string" ? new Request(request, init) : request).url
@@ -27,7 +27,7 @@ function checkURL(request, init) {
 }
 var urls;
 var init_checked_fetch = __esm({
-  ".wrangler/tmp/bundle-Ehbdbw/checked-fetch.js"() {
+  ".wrangler/tmp/bundle-eDem2s/checked-fetch.js"() {
     urls = /* @__PURE__ */ new Set();
     __name(checkURL, "checkURL");
     globalThis.fetch = new Proxy(globalThis.fetch, {
@@ -2239,11 +2239,11 @@ var init_database = __esm({
   }
 });
 
-// .wrangler/tmp/bundle-Ehbdbw/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-eDem2s/middleware-loader.entry.ts
 init_checked_fetch();
 init_modules_watch_stub();
 
-// .wrangler/tmp/bundle-Ehbdbw/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-eDem2s/middleware-insertion-facade.js
 init_checked_fetch();
 init_modules_watch_stub();
 
@@ -26857,7 +26857,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-Ehbdbw/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-eDem2s/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -26891,7 +26891,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-Ehbdbw/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-eDem2s/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
